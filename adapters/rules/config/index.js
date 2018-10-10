5059f20ddbb82fd33a7fdbdbb80d24e4d340af6b
Max Grossman <m.grossman1813@gmail.com>
2018-10-10 14:44:50 -0400

    update gitignore to push needed rules dir
    ref #1

---
+++ adapters/rules/config/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const buildTagChecks = require('./tagChecks');
+const buildDisabledFeatureChecks = require('../../disabledFeatures');
+
+const translateOsmType = require('../../helpers').translateOsmType;
+const flattenElements = require('../../helpers').flattenElements;
+const inferJosmGeometries = require('../../josmPresets/helpers').inferJosmGeometries;
+
+/**
+ * Provided presetConfig, replies rules used to build MapCSS
+ * replies a single mapcss rule config.
+ * @param {Object} config presetConfig
+ * @return {Object} single mapcss rule config.
+ */
+module.exports = (config) => {
+    const configRules = config.presets.map((preset) => {
+        const rules = preset.fields.map((field) => {
+            return buildTagChecks(field, preset.primary, preset.name, config.name);
+        });
+
+        return inferJosmGeometries(preset.geometry).map((geometry) => {
+            return {
+                osmType: geometry,
+                rules: flattenElements(rules)
+            };
+        });
+    });
+
+    if (config.hasOwnProperty('disabledFeatures')) {
+        const disabledFeatures = ['node', 'way'].map((geom) => {
+            return buildDisabledFeatureChecks(config.disabledFeatures, geom, config.name);
+        });
+        configRules.concat(disabledFeatures);
+    }
+
+    return flattenElements(configRules);
+};
