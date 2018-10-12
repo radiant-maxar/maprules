97d7409258897076314d08a589043dbc0406a706
Max Grossman <maxgrossman@users.noreply.github.com>
2018-10-12 13:18:49 -0400

    4 (#9)
    * update stale links
    
    ref #1
    
    * fix instructions
    
    ref #1
    
    * fix build script
    
    ref #1
    
    * update readmen
    
    ref #1
    
    * update gitignore to push needed rules dir
    
    ref #1
    
    * remove maprules-ui artifacts
    
    * pluck fontawesome
    
    ref #4

--- build.js
+++ build.js
@@ -5,6 +5,7 @@ const colors = require('colors/safe');
 const wd = process.cwd();
 
 function build() {
+
     // build docs
     shell.exec('npm run makedocs');
     console.log(colors.rainbow('\nDOCS BUILT!!\n\n'));    
