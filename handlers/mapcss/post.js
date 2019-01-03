dc7b4854647dc4ec210d1d09766a5602b9503688
Clarisse Abalos <samantha.abalos@digitalglobe.com>
2019-01-03 15:41:34 -0500

    fix sp error of usableMapCSS

--- handlers/mapcss/post.js
+++ handlers/mapcss/post.js
@@ -6,14 +6,14 @@ module.exports = async (r, h) => {
     try {
         const mapcss = r.payload;
         const rulesMapCSS = parseMapCSS(mapcss);
-        const usabledMapCSS = rulesMapCSS.every(obj => {
+        const usableMapCSS = rulesMapCSS.every(obj => {
             const keys = Object.keys(obj);
             return [/geometry/, /(not)?equals/, /error|warning/].every(requiredKey => {
                 return keys.findIndex(key => requiredKey.test(key)) >= 0;
             });
         });
 
-        if (!usabledMapCSS) throw new Error();
+        if (!usableMapCSS) throw new Error();
 
         return h.response(rulesMapCSS).header('Content-Type', 'application/json').code(200);
     } catch (error) {
