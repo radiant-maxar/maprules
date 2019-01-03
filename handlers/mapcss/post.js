fa15dd0f77b6ee128b86c332e9d58e48c3459b9f
Clarisse Abalos <samantha.abalos@digitalglobe.com>
2019-01-03 15:00:04 -0500

    attempt to add regex check in mapcss post handler

--- handlers/mapcss/post.js
+++ handlers/mapcss/post.js
@@ -6,8 +6,12 @@ module.exports = async (r, h) => {
     try {
         const mapcss = r.payload;
         const rulesMapCSS = parseMapCSS(mapcss);
-        
-        return h.response(rulesMapCSS).header('Content-Type', 'application/json').code(200);
+        rulesMapCSS.includes(function(rule){
+			if (![/geometry/, /equals/, /throw|error/].every((required => rule.includes(key => required.test(key))))) {
+			    throw new Error();
+			}
+        });
+	    return h.response(rulesMapCSS).header('Content-Type', 'application/json').code(200);
 
     } catch (error) {
         return Boom.boomify(error, { statusCode: 400 });
