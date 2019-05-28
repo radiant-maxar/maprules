6dc3953a5d695dbcd99ffda9e90aa4c3a9e8bfad
Max Grossman <m.grossman1813@gmail.com>
2019-05-28 11:35:21 -0400

    add new / update existing migration scripts. clean package depenedencies... ref #52

---
+++ migrations/20190524162348_user_sessions.js
@@ -0,0 +1,12 @@
+
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('user_sessions', function (table) {
+        table.string('id').primary().notNullable();
+        table.integer('user_id').unsigned().notNullable();
+        table.foreign('user_id').references('id').inTable('users');
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('user_sessions');
+};
--- migrations/20190524162659_preset_ownership.js
+++ migrations/20190524162659_preset_ownership.js
@@ -1,35 +1,58 @@
 
 exports.up = function (knex, Promise) {
-    // 1. add the user_id column
-    // 2. update all presets currently in table with public user...
-    // 3. add foreign key constraint so future presets have to have a user!
-    knex.schema.raw(`
-        ALTER TABLE presets ADD user_id INTEGER;
-        UPDATE presets SET user_id=0 WHERE user_id IS NULL;
-        ALTER TABLE ADD CONSTRAINT FOREIGN_KEY(user_id) REFERENCES users(id);
-    `)
+    // 1. make a temp database _presets_old;
+    return knex.schema.renameTable('presets', '_presets_old')
+        .then(function () {
+            return knex.schema.alterTable('_presets_old', function (table) {
+                // 2. initialize for all existing presets the 'public' user id
+                table.integer('user_id').defaultTo(0);
+            });
+        })
+        .then(function () { // 3. create the updated presets table with the user_id column
+            // return knex.schema.raw(
+            //     `CREATE TABLE 'presets'(
+            //         'id' uuid not null primary key,
+            //         'preset' JSON1,
+            //         'user_id' INTEGER DEFAULT 0,
+            //         FOREIGN_KEY(user_id) REFERENCES users(id)
+            //     )`
+            // );
+            return knex.schema.createTable('presets', function (table) {
+                table.uuid('id').notNullable();
+                table.json('preset');
+                table.integer('user_id').defaultTo(0).notNullable();
+                table.foreign('user_id').references('id').inTable('users');
+            });
+        })
+        .then(function () {
+            return knex('presets').del();
+        })
+        .then(function () { // 4. insert all that's in the temp table into the presets table
+            return knex.raw('INSERT INTO presets SELECT * from _presets_old;');
+        })
+        .then(function () { // 5. drop the temporary table...
+            return knex.schema.dropTableIfExists('_presets_old');
+        })
+        .catch(function (error) { throw error; });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
     // 1. copy presets to temp table
-    // 2. make presets with just id and presets column
-    // 3. insert just id and presets columns back into presets
-    // 4. drop the temp table..
-    knex.schema.raw(`
-        PRAGMA foreign_keys=off;
-
-        BEGIN TRANSACTION;
-
-        ALTER TABLE presets RENAME TO _presets_old;
-        CREATE TABLE presets (
-            \'id\' uuid not null primary key,
-            \'preset\' JSON1
-        );
-
-        INSERT INTO presets SELECT id, preset FROM _presets_old;
-
-        DROP TABLE IF EXISTS _presets_old;
-        COMMIT;
-        PRAGMA foreign_keys=on;
-    `);
+    return knex.schema.renameTable('presets', '_presets_old')
+        .then(function () { // 2. make presets with just id and presets column
+            return knex.schema.createTable('presets', function (table) {
+                table.uuid('id').notNullable();
+                table.json('preset');
+            });
+        })
+        .then(function () {
+            return knex('presets').del();
+        })
+        .then(function () { // 3. copy meaningful temp data back into presets table.
+            return knex.raw('INSERT INTO presets select id, preset from _presets_old');
+        })
+        .then(function () { // 4. drop the temp table..
+            return knex.schema.dropTableIfExists('_presets_old');
+        });
 };
+
