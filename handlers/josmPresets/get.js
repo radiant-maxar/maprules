3a931857aef348cf2f745b2eac98e2abd2999425
Max Grossman <m.grossman1813@gmail.com>
2019-06-06 15:31:18 -0400

    get rid of handlers!!
    ref #61

--- handlers/josmPresets/get.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const db = require('../../connection');
-
-const adaptJosmPresets = require('../../adapters').josmPresets;
-const mergePrimaries = require('../../adapters/serialize').mergePrimaries;
-
-// module.exports =
-// };
