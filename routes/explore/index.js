57f244712166201938c57adc15465348a0532838
Max Grossman <m.grossman1813@gmail.com>
2019-08-13 19:29:39 -0400

    include preset name. ref #65

--- routes/explore/index.js
+++ routes/explore/index.js
@@ -22,11 +22,11 @@ module.exports = {
                         // - find client to be who we think they are, return the user object
                         return Promise.resolve(r.state.maprules_session)
                             .then(function(session) {
-                                let token = jwt.verify(r.state.maprules_session);
+                                let token = jwt.verify(r.state.maprules_session, config.jwt);
                                 return isAuthorized(token, r.headers['user-agent']);
                             })
                             .then(function(session) {
-                                return { id: session.user_id };
+                                return { id: Number(session.id) };
                             })
                             .catch(function(error) {
                                 return {};
@@ -38,13 +38,25 @@ module.exports = {
                 const user = r.pre.sessionValidation;
 
                 // get preset id, user id, and user name for each preset
-                return db('presets').select('presets.id', 'presets.user_id', 'users.name')
+                return db('presets')
+                    .select({
+                        id: 'presets.id',
+                        preset: 'presets.preset',
+                        user_id: 'users.id',
+                        user_name: 'users.name'
+                    })
                     .innerJoin('users', 'users.id', '=', 'presets.user_id')
                     .then(function(results) {
-                        let presets = results.map(function(config) {
-                            config.edit = config.user_id === user.id;
-                            return config;
-                        });
+                        let presets = results.reduce(function(configs, config) {
+                            configs.push({
+                                preset_name: JSON.parse(config.preset).name,
+                                id: config.id,
+                                user_id: config.user_id,
+                                user_name: config.user_name,
+                                edit: config.user_id === user.id
+                            });
+                            return configs;
+                        }, []);
 
                         return h.response(presets).code(200);
                     })
